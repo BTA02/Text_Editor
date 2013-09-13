90d2064df2cf83f59ab2b6377ed5c9b072cb5875
BTA02 <andrew.axtell@gmail.com>
2013-09-13 11:36:51 -0400

    Friday Morning

--- TextEditor/gen/local/texteditor/R.java
+++ TextEditor/gen/local/texteditor/R.java
@@ -21,24 +21,33 @@ public final class R {
         public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_action_search=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
         public static final int action_settings=0x7f080001;
         public static final int edit_message=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_display_message=0x7f030000;
+        public static final int activity_main=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int display_message=0x7f070000;
+        public static final int main=0x7f070001;
     }
     public static final class string {
-        public static final int action_settings=0x7f040001;
+        public static final int action_settings=0x7f040005;
         public static final int app_name=0x7f040000;
-        public static final int edit_message=0x7f040003;
-        public static final int hello_world=0x7f040002;
-        public static final int send_button=0x7f040004;
+        public static final int edit_redo=0x7f04000a;
+        public static final int edit_undo=0x7f040009;
+        public static final int hello_world=0x7f040001;
+        public static final int menu_settings=0x7f040002;
+        public static final int send_button=0x7f040007;
+        public static final int start_typing=0x7f040006;
+        public static final int title_activity_display_message=0x7f040008;
+        public static final int title_activity_main=0x7f040003;
+        public static final int url=0x7f040004;
     }
     public static final class style {
         /** 
--- TextEditor/src/local/texteditor/MainActivity.java
+++ TextEditor/src/local/texteditor/MainActivity.java
@@ -1,12 +1,16 @@
 package local.texteditor;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.inputmethodservice.KeyboardView.OnKeyboardActionListener;
+import android.os.Bundle;
 import android.view.Menu;
-import android.view.View;
+import android.widget.EditText;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnKeyboardActionListener {
 
+	public final static String EXTRA_MESSAGE = "local.myfirstapp.message";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -19,9 +23,70 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+
+	@Override
+	public void onKey(int primaryCode, int[] keyCodes) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onPress(int primaryCode) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onRelease(int primaryCode) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onText(CharSequence text) { //in theory, this takes what was typed on the keyboard, and sends it to
+		//DisplayMessageActivity WITHOUT hitting an extra button
+		Intent intent = new Intent(this, DisplayMessageActivity.class);
+		EditText editText = (EditText) findViewById(R.id.edit_message);
+		String message = editText.getText().toString();
+		intent.putExtra(EXTRA_MESSAGE, message);
+		startActivity(intent);
+	}
+
+	@Override
+	public void swipeDown() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void swipeLeft() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void swipeRight() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void swipeUp() {
+		// TODO Auto-generated method stub
+		
+	}
 	
+
+	
+	/* For reference later
 	public void sendMessage (View view) {
 		//Do something in response to button
+		Intent intent = new Intent(this, DisplayMessageActivity.class);
+		EditText editText = (EditText) findViewById(R.id.edit_message);
+		String message = editText.getText().toString();
+		intent.putExtra(EXTRA_MESSAGE, message);
+		startActivity(intent);
 	}
+	*/
 
 }
